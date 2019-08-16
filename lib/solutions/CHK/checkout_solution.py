00b25e62f159681bb6c74642920dd2071294d896
sbx_user1051 <sbx_user1051@169.254.46.245>
2019-08-16 08:38:08 +0000

    Fri Aug 16 08:38:08 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,31 @@
+from collections import Counter
 
+PRICE_TABLE = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+DEALS = {
+    "A": (3, 130),
+    "B": (2, 45),
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    if not isinstance(skus, str):  # invalid input, wrong type
+        return -1
+
+    products = Counter(skus)
+
+    if len(products.keys() - PRICE_TABLE.keys()):  # unknown product
+        return -1
+
+    price = 0
+
+    for product, count in 
+
     raise NotImplementedError()
+
