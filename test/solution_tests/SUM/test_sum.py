9efaa8e0d9089019fc4e9354e1b50d0a73c8405a
sbx_user1051 <sbx_user1051@169.254.46.245>
2019-08-16 08:24:59 +0000

    Fri Aug 16 08:24:59 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,10 +1,12 @@
+import decimal
+
 import pytest
 
 from solutions.SUM import sum_solution
 
 
 @pytest.mark.parametrize("invalid_value", [
-    -1, 101, 1.0, "1", None, object()
+    -1, 101, 1.0, "1", None, object(), decimal.Decimal(10),
 ])
 def test_invalid_inputs(invalid_value):
     # tests invalid inputs on both x and y - 1 is valid
@@ -20,11 +22,13 @@ def test_invalid_inputs(invalid_value):
     (100, 101),
 ])
 def test_limit_values(limit_value, result):
+    # tests limit values for both inputs, 0 and 100, previous test goes one bellow or above
     assert sum_solution.compute(1, limit_value) == result
     assert sum_solution.compute(limit_value, 1) == result
 
 
-class TestSum:
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+def test_sum():
+    assert sum_solution.compute(1, 2) == 3
+    assert sum_solution.compute(2, 1) == 3
+
 
