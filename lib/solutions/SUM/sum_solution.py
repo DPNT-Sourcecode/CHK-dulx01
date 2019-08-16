69a86e743c6c10060ae7544e5a9e6ed50c0a3b17
sbx_user1051 <sbx_user1051@169.254.46.245>
2019-08-16 08:20:22 +0000

    Fri Aug 16 08:20:22 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if not isinstance(x, int):
+        raise ValueError("Unsupported type of x")
+    if not isinstance(y, int):
+        raise ValueError("Unsupported type of y")
+    if 0 > x or x > 100:
+        raise ValueError("Argument x isn't in the correct range")
+    if 0 > y or y > 100:
+        raise ValueError("Argument x isn't in the correct range")
+
+    return x + y
+
